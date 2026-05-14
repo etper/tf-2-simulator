fadd5fe79d8ca523673ee1215415fb0d02927b40
etper <piotr.brzezinski9@gmail.com>
2026-05-14 14:08:10 +0200

    Update inventory.gd

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -11,3 +11,19 @@ func add_item(item_id : String, amount := 1):
 		"id": item_id,
 		"amount": amount
 	})
+
+func remove_item(index : int):
+	if index >= 0 and index < items.size():
+		items.remove_at(index)
+
+func clear_inventory():
+	items.clear()
+
+func print_inventory():
+	print("=== INVENTORY ===")
+
+	for item in items:
+		var data = ItemDatabase.get_item(item["id"])
+
+		if data:
+			print(data.display_name)
