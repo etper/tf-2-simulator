e894c2db31c7ec3b9de5ebf7d31127c333f58a34
etper <piotr.brzezinski9@gmail.com>
2026-05-14 16:55:47 +0200

    item tooltip bg resize

--- scripts/item_tooltip.gd
+++ scripts/item_tooltip.gd
@@ -3,6 +3,8 @@ extends Panel
 @onready var name_label = $VBoxContainer/NameLabel
 @onready var description_label = $VBoxContainer/DescriptionLabel
 @onready var stats_label = $VBoxContainer/StatsLabel
+@onready var bg = $ColorRect
+@onready var vbox = $VBoxContainer
 
 func set_item(item : ItemData):
 
@@ -19,6 +21,12 @@ func set_item(item : ItemData):
 		stats += str(key) + ": " + str(item.metadata[key]) + "\n"
 
 	stats_label.text = stats
+	
+	vbox.reset_size()
+
+	await get_tree().process_frame
+
+	bg.size = vbox.get_combined_minimum_size() + Vector2(24, 24)
 
 func _process(delta):
 
