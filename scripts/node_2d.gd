f2460fc3a12393a866a3ba9c9dae63f910f3b668
etper <piotr.brzezinski9@gmail.com>
2026-05-14 13:53:01 +0200

    backpack ui slots done

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var slot_scene : PackedScene
 @export var slot_count := 50
 
-@onready var grid = $"../GridContainer"
+@onready var grid = $"../CenterContainer/GridContainer"
 
 func _ready():
 	for i in range(slot_count):
